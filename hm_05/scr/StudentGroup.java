f802589fe0fc03f0350c13a491ff183588915a5d
Stoyan SV <stoyan.vitekov@gmail.com>
2021-02-07 17:57:01 +0200

    update

--- hm_05/scr/StudentGroup.java
+++ hm_05/scr/StudentGroup.java
@@ -3,5 +3,31 @@ public class StudentGroup {
     Student[] students;
     int freePlaces;
 
+    StudentGroup() {
+        students = new Student[5];
+        freePlaces = 5;
+    }
+
+    StudentGroup(String subject) {
+        this();
+        this.groupSubject = subject;
+    }
+
+    void addStudent(Student s) {
+        if (s.subject.equals(groupSubject) && freePlaces > 0) {
+            students[students.length - 1] = s;
+            freePlaces--;
+        }
+    }
+
+    void emptyGroup() {
+        new StudentGroup();
+    }
+
+    String theBestStudent(){
+
+    }
+
 
 }
+
---
+++ hm_05/scr/Tempo.java
@@ -0,0 +1,2 @@
+public class Tempo {
+}
