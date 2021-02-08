b150189e62b37aaba05a592609211292e5b91aa3
Stoyan SV <stoyan.vitekov@gmail.com>
2021-02-08 15:52:30 +0200

    final homework 5

--- hm_05/scr/CollageDemo.java
+++ hm_05/scr/CollageDemo.java
@@ -1,21 +1,18 @@
-/**
- * created by teodor.zdravkov on 07/02/2021.
- */
-
 public class CollageDemo {
     public static void main(String[] args) {
         Student bobo = new Student("Bob", "History", 17);
         //bobo.grade = 4;
         Student ben = new Student("Ben", "History", 18);
-        ben.grade = 5;
+        ben.grade = 5.5;
         Student bil = new Student("Bil", "History", 19);
         bil.grade = 6;
         Student fil = new Student("Fil", "History", 17);
         fil.grade = 2;
         Student john = new Student("John", "History", 17);
-        john.grade = 1;
+        john.grade = 3.5;
         Student marry = new Student("Marry", "History", 17);
-        marry.grade =7;
+        marry.grade = 5;
+        Student andy = new Student("Andy","Math",20);
 
         StudentGroup history = new StudentGroup("History");
         System.out.println(history.freePlaces);
@@ -25,31 +22,37 @@ public class CollageDemo {
         history.addStudent(fil);
         history.addStudent(john);
         history.addStudent(marry);
-        System.out.println(history.freePlaces);
-        //history.emptyGroup();
-        System.out.println(history.freePlaces);
+        history.addStudent(andy);
+        System.out.println("Remaining places: " + history.freePlaces);
         history.printStudentsInGroup();
-        history.emptyGroup();
-//        System.out.println(history.freePlaces);
-//        history.printStudentsInGroup();
 
-        System.out.println();
-        StudentGroup sports = new StudentGroup("Sports");
-        Student gogo = new Student("Gogo", "Sports", 17);
-        gogo.grade = 3;
-        Student ivan = new Student("Ivan", "Sports", 18);
-        ivan.grade = 2;
-        Student pesho = new Student("Pesho", "Sports", 19);
-        pesho.grade = 6;
-        Student stoyan = new Student("Stoyan", "Sports", 19);
-        pesho.grade = 5;
-        Student sevo = new Student("Sevo", "Sports", 19);
-        pesho.grade = 4;
-        sports.addStudent(gogo);
-        sports.addStudent(ivan);
-        sports.addStudent(pesho);
-        sports.addStudent(stoyan);
-        sports.addStudent(sevo);
-        sports.printStudentsInGroup();
+        history.theBestStudent();
+        history.emptyGroup();
+        System.out.println("Remaining places after emptyGroup: " + history.freePlaces);
+
+
+//        history.printStudentsInGroup();
+//        history.emptyGroup();
+//        System.out.println(history.freePlaces);
+
+//
+//        System.out.println();
+//        StudentGroup sports = new StudentGroup("Sports");
+//        Student gogo = new Student("Gogo", "Sports", 17);
+//        gogo.grade = 3;
+//        Student ivan = new Student("Ivan", "Sports", 18);
+//        ivan.grade = 2;
+//        Student pesho = new Student("Pesho", "Sports", 19);
+//        pesho.grade = 6;
+//        Student stoyan = new Student("Stoyan", "Sports", 19);
+//        pesho.grade = 5;
+//        Student sevo = new Student("Sevo", "Sports", 19);
+//        pesho.grade = 4;
+//        sports.addStudent(gogo);
+//        sports.addStudent(ivan);
+//        sports.addStudent(pesho);
+//        sports.addStudent(stoyan);
+//        sports.addStudent(sevo);
+//        sports.printStudentsInGroup();
     }
 }
--- hm_05/scr/StudentGroup.java
+++ hm_05/scr/StudentGroup.java
@@ -15,10 +15,20 @@ public class StudentGroup {
     }
 
     void addStudent(Student s) {
-        if (s.subject.equals(groupSubject) && freePlaces > 0) {
-            students[freePlaces-1] = s;
-            freePlaces--;
+        if (!s.subject.equals(groupSubject)) {
+            System.out.println("This is not your specialty!");
+            return;
         }
+        if(freePlaces < 1){
+            System.out.println("There's no free places!");
+            return;
+        }
+        students[freePlaces-1] = s;
+        freePlaces--;
+//        if (s.subject.equals(groupSubject) && freePlaces > 0) {
+//           students[freePlaces-1] = s;
+//           freePlaces--;
+//        }
     }
 
     void emptyGroup() {
@@ -26,15 +36,24 @@ public class StudentGroup {
         this.freePlaces = 5;
     }
 
-    void printStudentsInGroup() {
-            //for(int i=0;i<students.length;i++){
-            for(Student student : students) {
-                System.out.println("Student name is: " + student.name + ", Student grade is: " + student.grade + " , Student group is: " + student.subject);
-                //System.out.println("Student name is: " + students[i].name + ", Student grade is: " + students[i].grade + " , Student group is: " + students[i].subject);
-            }
+    void theBestStudent(){
+        Student maxScore = students[0];
+        for(int i = 0; i < students.length; i++){
+            if (students[i].grade > maxScore.grade)
+        maxScore = students[i];
+        }
+        System.out.println("Student with highest score is: " + maxScore.name + " : " + maxScore.grade);
+
     }
 
+    void printStudentsInGroup() {
+            for(int i=0;i<students.length;i++){
+                System.out.println("Student name is: " + students[i].name + ", Student grade is: " + students[i].grade + " , Student group is: " + students[i].subject);
+            //for(Student student : students) {
+                //System.out.println("Student name is: " + student.name + ", Student grade is: " + student.grade + " , Student group is: " + student.subject);
 
+            }
+    }
 
 }
 
